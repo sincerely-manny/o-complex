49d243a66372d6dff98d7358be536efa3d1bb499
Kirill <sincerely.manny@gmail.com>
2024-04-11 11:33:52 +0300

    lint, prettier setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,65 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": true
-  },
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "extends": [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked"
-  ],
-  "rules": {
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        "prefer": "type-imports",
-        "fixStyle": "inline-type-imports"
-      }
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: ['./tsconfig.json'],
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'airbnb',
+        'airbnb-typescript',
+        'next/core-web-vitals',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'prettier',
     ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        "argsIgnorePattern": "^_"
-      }
-    ],
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      {
-        "checksVoidReturn": {
-          "attributes": false
-        }
-      }
-    ]
-  }
-}
+    rules: {
+        indent: 'off',
+        'no-plusplus': 'off',
+        '@typescript-eslint/indent': 'off',
+        '@typescript-eslint/array-type': 'off',
+        '@typescript-eslint/consistent-type-definitions': 'off',
+        '@typescript-eslint/consistent-type-imports': [
+            'warn',
+            {
+                prefer: 'type-imports',
+                fixStyle: 'inline-type-imports',
+            },
+        ],
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        // '@typescript-eslint/require-await': 'off',
+        '@typescript-eslint/no-misused-promises': [
+            'error',
+            {
+                checksVoidReturn: { attributes: false },
+            },
+        ],
+        'react/require-default-props': [
+            'error',
+            {
+                forbidDefaultForRequired: true,
+                functions: 'defaultArguments',
+            },
+        ],
+        'no-restricted-syntax': [
+            'error',
+            {
+                selector: 'ForInStatement',
+                message:
+                    'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+            },
+            {
+                selector: 'LabeledStatement',
+                message:
+                    'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+            },
+            {
+                selector: 'WithStatement',
+                message:
+                    '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+            },
+        ],
+    },
+};
+
 module.exports = config;
--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+// eslint-disable-next-line import/extensions
+await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {};
